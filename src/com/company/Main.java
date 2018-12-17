d18647f34b932e47b1ce268f31269ddfe3b2b95e
jb838 <jacob.blasek1220@gmail.com>
2018-12-17 11:20:15 -0500

    we really out here

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	String array [] = new String [9];
+        for(int i = 0; i < 9; i++) {
+            array[i] = " ";
+        }
+        array[0] = "x";
+            for(int i = 0; i < 9; i++) {
+                if(i % 3 == 0 && i != 0) {
+                    System.out.println("\n" + "-------------");
+                }
+                    System.out.print(array[i]);
+                if(i == 2 || i == 5 || i == 8) {
+                    int nothingHappens;
+                }
+                else {
+                    System.out.print("\t" +  "|");
+                }
+        }
+
+
+
     }
 }
